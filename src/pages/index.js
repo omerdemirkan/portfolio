5dfdf53faeaa2ab9f1c15d14ea27b239607ff7d3
Omer Demirkan <omerfarukpiano@gmail.com>
2020-02-20 11:04:54 -0800

    set up basic pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => {
+    return <div>
+        <h1>Hi</h1>
+        <h1>I'm Omer</h1>
+        <h2>I’m a full-stack developer based in Los Angeles and I specialize in building performant web applications.</h2>
+    </div>
+}
