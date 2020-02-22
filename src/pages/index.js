ac7d10fb3aebe6a4bcbd5288343a4a9f47fe43cd
Omer Demirkan <omerfarukpiano@gmail.com>
2020-02-22 14:44:55 -0800

    import all needed svg's

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Layout from '../components/Layout/Layout';
 
 import classes from './styles/index.module.css';
 
+import reactLogo from '../images/react.svg';
+
 export default () => {
     return <Layout>
         <div className={classes.Hero}>
@@ -11,6 +13,11 @@ export default () => {
                 <h1>Hi, I'm Omer</h1>
                 <p>I’m a full-stack developer based in Los Angeles and I specialize in building performant web applications.</p>
 
+            </div>
+        </div>
+        <div className={classes.SkillsSection}>
+            <div className={classes.SkillBox}>
+                <img src={reactLogo} className={classes.ReactLogo}/>
             </div>
         </div>
         
