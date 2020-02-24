f9019f602d8dbe5fd60a4e282bc269c3ff10248c
Omer Demirkan <omerfarukpiano@gmail.com>
2020-02-23 16:46:05 -0800

    remove all svg's

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import classes from './Navbar.module.css';
 
 import { Link } from 'gatsby';
 
 export default function Navbar() {
-    return <div className={classes.Navbar}>
+
+    // Changing background based on scroll position
+
+    const [navBackground, setNavBackground] = useState(false);
+    const navRef = useRef();
+    navRef.current = navBackground;
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const show = window.scrollY > 60
+            if (navRef.current !== show) {
+              setNavBackground(show)
+            }
+        }
+        document.addEventListener('scroll', handleScroll)
+        return () => {
+        document.removeEventListener('scroll', handleScroll)
+        }
+    }, []);
+
+    return <div className={classes.Navbar} style={navBackground ? {backgroundColor: 'rgb(219, 219, 219)'}: null}>
         <h2 className={classes.Logo}><Link className={classes.LogoLink} to='/'>Omer Demirkan</Link></h2>
         <ul className={classes.NavList}>
-            <li className={classes.NavListItem}><Link to='/' className={classes.NavLink}>Home</Link></li>
-            <li className={classes.NavListItem}><Link to='/contact' className={classes.NavLink}>Contact Me</Link></li>
+            <li className={classes.NavListItem}><Link activeClassName={classes.ActiveLink} to='/' className={classes.NavLink}>Home</Link></li>
+            <li className={classes.NavListItem}><Link activeClassName={classes.ActiveLink} to='/contact' className={classes.NavLink}>Contact Me</Link></li>
         </ul>
-        
-        
     </div>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,6 @@ import Layout from '../components/Layout/Layout';
 
 import classes from './styles/index.module.css';
 
-import languages from '../images/languages.svg';
-import frontend from '../images/front-end-frameworks.svg';
-import backend from '../images/back-end-frameworks.svg';
-
 export default () => {
     return <Layout>
         <div className={classes.Hero}>
@@ -14,25 +10,6 @@ export default () => {
             <div className={classes.HeroTextBox}>
                 <h1>Hi, I'm Omer</h1>
                 <p>I’m a full-stack developer based in Los Angeles and I specialize in building performant web applications.</p>
-
-            </div>
-        </div>
-
-        <div className={classes.SkillsSection}>
-            <h1 className={classes.SkillsSectionHeader}>What do I know?</h1>
-
-            <div className={classes.SkillBox}>
-                <img src={languages} className={classes.Logos}/>
-
-            </div>
-                
-            <div className={classes.SkillBox}>
-                <img src={frontend} className={classes.Logos}/>
-
-            </div>
-
-            <div className={classes.SkillBox}>
-                <img src={backend} className={classes.Logos}/>
 
             </div>
         </div>
