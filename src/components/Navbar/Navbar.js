f10de803a336dee97a75737973946cd43c6e010c
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-12 20:20:14 -0700

    reorganize images folder

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -24,7 +24,7 @@ export default function Navbar() {
         }
     }, []);
 
-    const logoText = '{OD}'
+    const logoText = '<Omer/>'
 
     return <div className={classes.Navbar} style={navBackground ? {backgroundColor: 'rgb(247, 247, 247)', height: '80px'}: null}>
         <h2 className={classes.Logo}><Link className={classes.LogoLink} to='/'>{logoText}</Link></h2>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ import Layout from '../components/Layout/Layout';
 import classes from './styles/index.module.css';
 
 // images
-import heroImage from '../images/hero-image.svg';
-import me from '../images/me.png';
-import waves from '../images/waves.svg';
+import heroImage from '../images/home-page/hero-image.svg';
+import me from '../images/home-page/me.png';
+import waves from '../images/home-page/waves.svg';
 
-import frontEnd from '../images/front-end.svg';
-import backEnd from '../images/back-end.svg';
-import languages from '../images/languages.svg';
+import frontEnd from '../images/skills/front-end.svg';
+import backEnd from '../images/skills/back-end.svg';
+import languages from '../images/skills/languages.svg';
 // import workoutshare from '../images/workoutshare.png';
 // import synonymy from '../images/synonymy.png';
 // import whattime from '../images/whattime.png';
@@ -28,7 +28,7 @@ export default () => {
                 <div>
                     <p>I am a Computer Science Student and Software Engineer based in Los Angeles, and I specialize in building performant web applications.</p>
                     <div className={classes.HeroButtonsBox}>
-                        <Scroll smooth={true} duration={500} offset={-80} to='projects-section'><button className={classes.MainHeaderButton}>See My Work</button></Scroll>
+                        <Scroll smooth={true} duration={500} offset={-200} to='projects-section'><button className={classes.MainHeaderButton}>See My Work</button></Scroll>
                         <Link to='/contact'><button className={classes.SecondaryHeaderButton}>Get In Touch</button></Link>
                     </div>
                 </div>
@@ -43,7 +43,7 @@ export default () => {
             <div className={classes.MePairBox}>
                 <img src={me}/>
                 <div>
-                    <p>I am a Turkish-American Computer Science student at Los Angeles Mission College and a hobbyist Full Stack Developer. I have a passion for building full fledged web applications from the bottom up, from simple and intuitive user interfaces to API design.</p>
+                    <p>I am a Turkish-American Computer Science student at Los Angeles Mission College and a hobbyist Full Stack Developer. I have a passion for building full fledged web applications from the bottom up: from simple and intuitive user interfaces to API design.</p>
                 </div>
             </div>
         </div>
@@ -63,7 +63,7 @@ export default () => {
                 </div>
 
                 <div className={classes.SkillBox}>
-                    <img src={frontEnd} className={classes.SkillImage} style={{}}/>
+                    <img src={frontEnd} className={classes.SkillImage} style={{position: 'relative', right: '4px'}}/>
                     <div>
                         <h3>Front-end Technologies</h3>
                         <p>React - Redux</p>
@@ -82,18 +82,18 @@ export default () => {
         </div>
 
 
-        <div id='projects-section'>
+        <div id='projects-section' className={classes.ProjectsSection}>
             <h1>Here's Some Of My Work</h1>
-            <div>
-                <div>
+            <div className={classes.ProjectsBox}>
+                <div className={classes.ProjectBox}>
                     {/* <img src={synonymy}/> */}
 
                 </div>
-                <div>
+                <div className={classes.ProjectBox}>
                     {/* <img src={whattime}/> */}
                     
                 </div>
-                <div>
+                <div className={classes.ProjectBox}>
                     {/* <img src={workoutshare}/> */}
                     
                 </div>
