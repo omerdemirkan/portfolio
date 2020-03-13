d142713f3f77951e8eac1016229f4d1a89d7c664
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-12 19:47:40 -0700

    add a responsive skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,11 @@ import classes from './styles/index.module.css';
 // images
 import heroImage from '../images/hero-image.svg';
 import me from '../images/me.png';
+import waves from '../images/waves.svg';
+
+import frontEnd from '../images/front-end.svg';
+import backEnd from '../images/back-end.svg';
+import languages from '../images/languages.svg';
 // import workoutshare from '../images/workoutshare.png';
 // import synonymy from '../images/synonymy.png';
 // import whattime from '../images/whattime.png';
@@ -24,7 +29,7 @@ export default () => {
                     <p>I am a Computer Science Student and Software Engineer based in Los Angeles, and I specialize in building performant web applications.</p>
                     <div className={classes.HeroButtonsBox}>
                         <Scroll smooth={true} duration={500} offset={-80} to='projects-section'><button className={classes.MainHeaderButton}>See My Work</button></Scroll>
-                        <Link to='/contact'><button className={classes.SecondaryHeaderButton}>Contact Me</button></Link>
+                        <Link to='/contact'><button className={classes.SecondaryHeaderButton}>Get In Touch</button></Link>
                     </div>
                 </div>
                 
@@ -38,9 +43,42 @@ export default () => {
             <div className={classes.MePairBox}>
                 <img src={me}/>
                 <div>
-                    <p>I am a Turkish-American Computer Science student at Los Angeles Mission College and a hobbyist Full Stack Developer. I have a passion for building full fledged web applications from the bottom up, from simple and intuitive user interfaces to database structures.</p>
+                    <p>I am a Turkish-American Computer Science student at Los Angeles Mission College and a hobbyist Full Stack Developer. I have a passion for building full fledged web applications from the bottom up, from simple and intuitive user interfaces to API design.</p>
                 </div>
             </div>
+        </div>
+
+        <div className={classes.SkillsSection}>
+            <img src={waves} className={classes.WavesImage}></img>
+            <h1>Here's What I'm Good At</h1>
+
+            <div className={classes.SkillsBox}>
+
+                <div className={classes.SkillBox}>
+                    <img src={languages} className={classes.SkillImage}/>
+                    <div>
+                        <h3>Languages</h3>
+                        <p>JavaScript (ES6) - HTML5 - CSS3</p>
+                    </div>
+                </div>
+
+                <div className={classes.SkillBox}>
+                    <img src={frontEnd} className={classes.SkillImage} style={{}}/>
+                    <div>
+                        <h3>Front-end Technologies</h3>
+                        <p>React - Redux</p>
+                    </div>
+                </div>
+
+                <div className={classes.SkillBox}>
+                    <img src={backEnd} className={classes.SkillImage}/>
+                    <div>
+                        <h3>Back-end Technologies</h3>
+                        <p>NodeJS - Express - MongoDB</p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
 
 
